d9271060c2c3e48f001a20021ab1a48e97baafc8
misakikondo <misakikondo718@gmail.com>
2020-10-31 14:38:31 +0900

    error messages

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :items_name
+    validates :persons_name
+    validates :memos
+    validates :places
+    validates :found_date
+  end
 end
